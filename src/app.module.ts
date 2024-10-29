fb33a75ad812ab6c6222a6687dcb6e6efde39c09
MaxiBakka <maxi.bakka@gmail.com>
2024-10-29 01:12:19 -0300

    add env app config files and database typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import databaseConfig from './database/config/database.config';
+import appConfig from './config/app.config';
+import mailConfig from './mail/config/mail.config';
+import fileConfig from './files/config/file.config';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const infrastructureDatabaseModule = TypeOrmModule.forRootAsync({
+  useClass: TypeOrmConfigService,
+  dataSourceFactory: async (options: DataSourceOptions) => {
+    return new DataSource(options).initialize();
+  },
+});
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig, mailConfig, fileConfig],
+      envFilePath: ['.env'],
+    }),
+    infrastructureDatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ src/database/config/database.config.ts
@@ -0,0 +1,67 @@
+import { registerAs } from '@nestjs/config';
+
+import {
+  IsOptional,
+  IsInt,
+  Min,
+  Max,
+  IsString,
+  ValidateIf,
+  IsBoolean,
+} from 'class-validator';
+import validateConfig from '../../utils/validate-config';
+import { DatabaseConfig } from './database-config.type';
+
+class EnvironmentVariablesValidator {
+  @ValidateIf((envValues) => envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_URL: string;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_TYPE: string;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_HOST: string;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsInt()
+  @Min(0)
+  @Max(65535)
+  DATABASE_PORT: number;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_PASSWORD: string;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_NAME: string;
+
+  @ValidateIf((envValues) => !envValues.DATABASE_URL)
+  @IsString()
+  DATABASE_USERNAME: string;
+
+  @IsBoolean()
+  @IsOptional()
+  DATABASE_SYNCHRONIZE: boolean;
+}
+
+export default registerAs<DatabaseConfig>('database', () => {
+  validateConfig(process.env, EnvironmentVariablesValidator);
+
+  return {
+    isDocumentDatabase: false,
+    url: process.env.DATABASE_URL,
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT
+      ? parseInt(process.env.DATABASE_PORT, 10)
+      : 3306,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+  };
+});
